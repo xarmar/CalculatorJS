9d931053ba63e0c2c71d62651da1451170234189
Xavier Marques <xavierpmarques@gmail.com>
2021-05-18 23:10:36 +0100

    add 'newInput' function that will tranform user Buttonclicks into numbers and operations

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -5,6 +5,7 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
     button.addEventListener("click", increaseOnClick);
+    button.addEventListener("click", newInput)
 });
 
 function increaseOnClick (e) {
@@ -17,5 +18,76 @@ function increaseOnClick (e) {
         clickedButton.classList.remove("increaseEffect")
     }
 }
+
+function newInput(e) {
+    let id = e.target.id;
+    switch (id) {
+        case zero:
+
+            break;  
+        case one:
+            
+            break;
+        case two:
+            
+            break;
+        case three:
+            
+            break;
+        case four:
+            
+            break;
+        case five:
+            
+            break;
+        case six:
+            
+            break;
+        case seven:
+            
+            break;
+        case eight:
+            
+            break;
+        case nine:
+            
+            break;
+        case signChange:
+            
+            break;
+        case decimal:
+            
+            break;
+        case equals:
+            
+            break;
+        case add:
+            
+            break;
+        case subtract:
+            
+            break;
+        case multiply:
+            
+            break;
+        case division:
+            
+            break;
+        case power:
+            
+            break;
+        case backspace:
+            
+            break;
+        case clear:
+            
+            break;
+        default:
+            break;
+    }
+    
+}
+
+
 
 })
